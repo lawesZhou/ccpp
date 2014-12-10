ffad3722b73972d281a5198d6869a42725c8f640
laosi <1124519074@qq.com>
2014-12-10 11:39:46 +0800

    update print.h

--- probability/print.h
+++ probability/print.h
@@ -1,7 +1,7 @@
 #ifndef PRINT_H
 #define PRINT_H
-#define printArray(x,n) _Generic((x), int    *: print_intArray,	\
-				  double *: print_doubleArray)(x, n)
+#define printArray(x,n) _Generic((x), int    *: print_intArray,		\
+				      double *: print_doubleArray)(x, n)
 
 void print_intArray(int *array, int n);
 
